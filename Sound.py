4ad2fad446d60134fdcc6e0e495097beec258fd5
khajan_bhatt <tanujbhatt8279@gmail.com>
2024-08-01 01:39:49 +0530

    Modified Sounds branch

---
+++ Sound.py
@@ -0,0 +1,14 @@
+from playsound import playsound
+def ADERROR():
+     playsound('Railway-Reservation-System\Sounds\Sorry, Wrong Password.mp3')
+     playsound('Railway-Reservation-System\Sounds\Access_Denied_Preview.mp3')
+def WELCOME():
+     playsound('Railway-Reservation-System\Sounds\Welcome to Great...mp3')
+def INPUT():
+     playsound('Railway-Reservation-System\Sounds\Invalid Input.mp3')
+def CONTQUIT():
+     playsound('Railway-Reservation-System\Sounds\Continue and quit.mp3')
+def THANKS():
+     playsound('Railway-Reservation-System\Sounds\Thanks.mp3')
+def FEEDBACK():
+     playsound('Railway-Reservation-System\Sounds\Feedback.mp3')
